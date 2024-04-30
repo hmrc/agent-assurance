6d66ba122119819880c937502e9e0bd26bf6848e
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2024-04-30 14:32:49 +0100

    [WG][APB-7596][APB8092] Mongo Cache for AgentRecord calls,fix testing

--- it/test/uk/gov/hmrc/agentassurance/controllers/AmlsDetailsByArnControllerISpec.scala
+++ it/test/uk/gov/hmrc/agentassurance/controllers/AmlsDetailsByArnControllerISpec.scala
@@ -79,7 +79,8 @@ class AmlsDetailsByArnControllerISpec
         "microservice.services.des.port"       -> wireMockPort,
         "auditing.enabled"                     -> false,
         "stride.roles.agent-assurance"         -> "maintain_agent_manually_assure",
-        "internal-auth-token-enabled-on-start" -> false
+        "internal-auth-token-enabled-on-start" -> false,
+        "agent.cache.enabled"                  -> false
       )
       .overrides(moduleWithOverrides)
 
