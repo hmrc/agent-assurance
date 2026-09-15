30a861db6c4b19a43e8b4a9d829da31a5354fa78
Iyad Issa <237692191+iscalit@users.noreply.github.com>
2026-09-15 07:03:34 +0100

    APB-11907: migrate agency caches to AES-GCM

--- app/uk/gov/hmrc/agentassurance/repositories/AgencyNameCacheRepository.scala
+++ app/uk/gov/hmrc/agentassurance/repositories/AgencyNameCacheRepository.scala
@@ -16,7 +16,6 @@
 
 package uk.gov.hmrc.agentassurance.repositories
 
-import com.codahale.metrics.MetricRegistry
 import play.api.Configuration
 import play.api.libs.json.Format
 import play.api.libs.json.Reads
@@ -24,13 +23,12 @@ import play.api.libs.json.Writes
 import uk.gov.hmrc.agentassurance.services.Cache
 import uk.gov.hmrc.crypto.Decrypter
 import uk.gov.hmrc.crypto.Encrypter
-import uk.gov.hmrc.crypto.PlainText
+import uk.gov.hmrc.crypto.json.JsonEncryption.stringEncrypterDecrypter
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.TimestampSupport
 import uk.gov.hmrc.mongo.cache.CacheIdType
 import uk.gov.hmrc.mongo.cache.EntityCache
 import uk.gov.hmrc.mongo.cache.MongoCacheRepository
-import uk.gov.hmrc.play.bootstrap.metrics.Metrics
 
 import javax.inject.Inject
 import javax.inject.Named
@@ -44,8 +42,7 @@ import scala.util.Success
 class AgencyNameCacheRepository @Inject() (
   config: Configuration,
   mongo: MongoComponent,
-  timestampSupport: TimestampSupport,
-  metrics: Metrics
+  timestampSupport: TimestampSupport
 )(
   implicit
   ec: ExecutionContext,
@@ -55,7 +52,11 @@ class AgencyNameCacheRepository @Inject() (
 extends EntityCache[String, Option[String]]
 with Cache[Option[String]]:
 
-  val format: Format[Option[String]] = Format(Reads.optionWithNull[String], Writes.optionWithNull[String])
+  private val encryptedStringFormat: Format[String] = stringEncrypterDecrypter(using crypto)
+  val format: Format[Option[String]] = Format(
+    Reads.optionWithNull[String](using encryptedStringFormat),
+    Writes.optionWithNull[String](using encryptedStringFormat)
+  )
   val cacheRepo: MongoCacheRepository[String] =
     new MongoCacheRepository(
       mongoComponent = mongo,
@@ -66,22 +67,16 @@ with Cache[Option[String]]:
       replaceIndexes = true
     )
 
-  val record: MetricRegistry = metrics.defaultRegistry
-
   def apply(
     key: String
   )(body: => Future[Option[String]])(implicit ec: ExecutionContext): Future[Option[String]] =
-    val encryptedKey = crypto.encrypt(PlainText(key)).value
-    getFromCache(encryptedKey).flatMap:
-      case Some(v) =>
-        record.counter(s"Count-$key-from-cache")
-        Future.successful(v)
+    getFromCache(key).flatMap:
+      case Some(v) => Future.successful(v)
       case None =>
         body.andThen:
           case Success(v) =>
-            record.counter(s"Count-$key-from-source")
             v.map(notNoneV =>
-              putCache(encryptedKey)(Some(notNoneV))
+              putCache(key)(Some(notNoneV))
                 .map(_ => Some(notNoneV))
             ).getOrElse(Future.successful(None))
   end apply
